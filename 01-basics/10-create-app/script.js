42f36edc1175ee0597374ba8c4bfa6b572578d02
Tagir <sulaev.tagir@mail.ru>
2025-03-06 03:48:46 +0300

    task create-app completed

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,16 @@
 import { defineComponent, createApp } from 'vue'
+
+const RootComponent = defineComponent({
+  name: 'RootComponent',
+  setup() {
+    const currentDate = new Date().toLocaleDateString('en-EN', { dateStyle: 'long' })
+
+    return {
+      currentDate,
+    }
+  },
+
+  template: `<div>Сегодня {{ currentDate }}</div>`,
+})
+
+createApp(RootComponent).mount('#app')
