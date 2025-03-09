faf26b2730b3146788c1994c6693e8775a753d7f
Tagir <sulaev.tagir@mail.ru>
2025-03-09 19:06:54 +0300

    task broken-map completed

--- 02-basics-2/20-broken-map/main.js
+++ 02-basics-2/20-broken-map/main.js
@@ -1,6 +1,6 @@
 import '@shgk/vue-course-ui/meetups/style.css'
 import './MapApp.css'
 import { createApp } from 'vue'
-import MapApp from './MapApp.js'
+import MapApp from './MapApp.vue'
 
 createApp(MapApp).mount('#app')
