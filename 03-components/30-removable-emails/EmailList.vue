15aca302dd9415f1ab8ebc1d04072a719f77d71e
Tagir <sulaev.tagir@mail.ru>
2025-03-12 14:50:53 +0300

    task removable-emails completed

---
+++ 03-components/30-removable-emails/EmailList.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import EmailListItem from './EmailListItem.vue'
+
+interface IEmail {
+  email: string
+  isMarked: boolean
+}
+
+defineProps<{
+  emails: IEmail[]
+}>()
+
+const emit = defineEmits<{
+    handleRemoveItem: [index: number]
+}>()
+</script>
+
+<template>
+  <ul class="emails-list unstyled-list" aria-label="Emails">
+    <EmailListItem
+      v-for="({ email, isMarked }, index) in emails"
+      :key="email"
+      :email="email"
+      :marked="isMarked"
+      :index="index"
+      @handle-remove="emit('handleRemoveItem', index)"
+    />
+  </ul>
+</template>
+
+<style scoped></style>
---
+++ 03-components/30-removable-emails/emails.ts
@@ -0,0 +1,30 @@
+// Значения взяты из https://jsonplaceholder.typicode.com/comments
+export function getEmails() {
+  return [
+    'Eliseo@gardner.biz',
+    'Jayne_Kuhic@sydney.com',
+    'Nikita@garfield.biz',
+    'Lew@alysha.tv',
+    'Hayden@althea.biz',
+    'Presley.Mueller@myrl.com',
+    'Dallas@ole.me',
+    'Mallory_Kunze@marie.org',
+    'Meghan_Littel@rene.us',
+    'Carmen_Keeling@caroline.name',
+    'Veronica_Goodwin@timmothy.net',
+    'Oswald.Vandervort@leanne.org',
+    'Kariane@jadyn.tv',
+    'Nathan@solon.io',
+    'Maynard.Hodkiewicz@roberta.com',
+    'Christine@ayana.info',
+    'Preston_Hudson@blaise.tv',
+    'Vincenza_Klocko@albertha.name',
+    'Madelynn.Gorczany@darion.biz',
+    'Mariana_Orn@preston.org',
+    'Noemie@marques.me',
+    'Khalil@emile.co.uk',
+    'Sophia@arianna.co.uk',
+    'Jeffery@juwan.us',
+    'Isaias_Kuhic@jarrett.net',
+  ]
+}
